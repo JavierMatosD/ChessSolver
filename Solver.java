d12668d2c49e623790e22805812a91b02206fbb3
Javier Matos <javier.a.matos@outlook.com>
2021-04-23 18:11:02 -0400

    Minor changes to comments. Added defined Enums

--- Solver.java
+++ Solver.java
@@ -1,3 +1,12 @@
+/**
+ * 
+ */
+
+enum ChessPiece
+{
+    W_CASTLE, W_KNIGHT, W_BISHOP, W_QUEEN, W_KING, W_PAWN, EMPTY,
+    B_CASTLE, B_KNIGHT, B_BISHOP, B_QUEEN, B_KING, B_PAWN,
+}
 public class Solver
 {
 
@@ -8,19 +17,29 @@ public class Solver
      */
     public static Enum[][] ReadAndConvert(String FILENAME)
     {
-        Enum[][] board = new Enum[8][8];
+        ChessPiece[][] board = new ChessPiece[8][8];
 
         return board;
     }
 
     /**
-     * 
+     * Legal moves for each piece
+     * Kings  : One square space in any direction as long as the square cannot be attacked by another piece.
+     *          Special move Kings Castling
+     * Queens : Diagonally, horizontally, or vertically any number of spaces. Cannot jump over pieces. 
+     * Rooks  : Horizontally or vertically any number of squares. They are Unable to jump over pieces. 
+     *          Rooks move when king castles.
+     * Bishops: Diagonally any number of squares. They are unable to jump over pieces.
+     * Knights: Move in an L shape: 2 squares horizontally or vertically. They can jump over pieces.
+     * Pawns  : Vertically forward one move unless they haven't moved before. 
+     *          Pawns Capture one square in forwards diagonal movement
      * @param 2D array of enums representing the chess board
      * @param boolean true if its whites turn
      * @return Array of Move's
      */
-    public static Move[] GetLegalMoves(Enum[][] board, Boolean turn)
+    public static Move[] GetLegalMoves(ChessPiece[][] board, Boolean turn)
     {
+
         Move[] legalMoves = new Move[16];
 
         return legalMoves;
@@ -33,7 +52,7 @@ public class Solver
      * @param int number of moves to Checkmate
      * @return Array of Moves for Checkmate
      */
-    public static Move[] SolvePuzzle(Enum[][] board, int numMoves)
+    public static Move[] SolvePuzzle(ChessPiece[][] board, int numMoves)
     {
         Move[] winningMoves = new Move[2];
 
@@ -44,6 +63,7 @@ public class Solver
 
     public static void main(String[] args) 
     {
-        Enum[][] board = ReadAndConvert("Puzzles/" + args[0]);
+        //Enum[][] board = ReadAndConvert("Puzzles/" + args[0]);
+        
     }
 }
